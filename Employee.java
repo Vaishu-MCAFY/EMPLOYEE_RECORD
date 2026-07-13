eb763c2f5bb53bdacee1837a0701744bc6bbb170
Vaishu-MCAFY <akangirevaishnavi2003@gmail.com>
2026-07-13 22:18:35 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,58 @@
+package model;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private int id;
+    private String name;
+    private String department;
+    private double salary;
+
+    // Default Constructor
+    public Employee() {
+    }
+
+    // Parameterized Constructor
+    public Employee(int id, String name, String department, double salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
